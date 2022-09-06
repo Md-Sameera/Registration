58ab3de28c1995bc98cdb3f4db743a42edf44e62
sameera <48850057+Md-Sameera@users.noreply.github.com>
2022-09-06 18:04:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Form from './Form';
+import Search from './Search';
 
 function App() {
   return (
     <>
-    
-
     <Form/>
-
+    <Search/>
     </>
   );
 }
 
 export default App;
+
